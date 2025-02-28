656803a934a45dcd15a78630daa4b03ce791972b
c1oud-dev <gksmf0187@gmail.com>
2025-02-28 17:50:42 +0900

    Project Create_20250228

---
+++ src/test/java/booktine/Booktine/BooktineApplicationTests.java
@@ -0,0 +1,13 @@
+package booktine.Booktine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
