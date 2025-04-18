2360dac3750afe4bc689a3572c476f03d9e0d06c
c1oud-dev <gksmf0187@gmail.com>
2025-04-18 13:19:31 +0900

    Adding Cors Global Settings to the WebConfig File

--- src/main/java/booktine/Booktine/config/WebConfig.java
+++ src/main/java/booktine/Booktine/config/WebConfig.java
@@ -1,7 +1,7 @@
 package booktine.Booktine.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
         // 예: 실제 "uploads" 폴더를 "/images/**" URL로 매핑
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:uploads/");
+    }
+    // CORS 전역 설정 추가
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+               .allowedOriginPatterns("https://*.github.io", "http://localhost:3000")
+               .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
