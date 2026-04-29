449e2b12d677f9613d7bceb7e206758238e7e1b2
c1oud-dev <gksmf0187@gmail.com>
2026-04-29 12:41:46 +0900

    feat(#4): MemoRepository 추가

---
+++ src/main/java/booktine/Booktine/domain/memo/repository/MemoRepository.java
@@ -0,0 +1,18 @@
+package booktine.Booktine.domain.memo.repository;
+
+import booktine.Booktine.domain.memo.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Memo 엔티티의 영속성 처리를 담당하는 리포지토리.
+ * 게시물별 메모 목록 조회 쿼리를 제공한다.
+ */
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    /**
+     * 게시물 ID 기준으로 연결된 메모를 모두 조회한다.
+     */
+    List<Memo> findAllByPostId(Long postId);
+}
