3226927a6dc886c0d60d2ae9c25ed54edf6a1fcb
c1oud-dev <gksmf0187@gmail.com>
2026-04-30 11:04:55 +0900

    feat(#5): AnnualGoal, MonthlyGoal 엔티티

---
+++ src/main/java/booktine/Booktine/domain/progress/entity/MonthlyGoal.java
@@ -0,0 +1,55 @@
+package booktine.Booktine.domain.progress.entity;
+
+import booktine.Booktine.domain.user.entity.User;
+import booktine.Booktine.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 사용자 월간 독서 목표를 저장하는 JPA 엔티티.
+ * Progress 도메인에서 월별 목표 CRUD 및 월별 달성 통계 비교에 사용된다.
+ */
+@Getter
+@Entity
+@Table(name = "monthly_goals")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MonthlyGoal extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private Integer year;
+
+    @Column(nullable = false)
+    private Integer month;
+
+    @Column(nullable = false)
+    private Integer goalCount;
+
+    /**
+     * 월간 목표 생성 시 필요한 필드를 초기화한다.
+     */
+    @Builder
+    public MonthlyGoal(User user, Integer year, Integer month, Integer goalCount) {
+        this.user = user;
+        this.year = year;
+        this.month = month;
+        this.goalCount = goalCount;
+    }
+
+    /**
+     * 월간 목표 수치를 수정한다.
+     */
+    public void updateGoalCount(Integer goalCount) {
+        this.goalCount = goalCount;
+    }
+}
